9aa67397c37f70040d4ffb750133f4cea3a5d7b2
Sahitya hp <neupanesahitya1@gmail.com>
2024-08-29 16:25:24 -0400

    app running in port

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,13 @@
 const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const cors = require("cors");
+
+const port = process.env.PORT || 301;
+
+app.use(cors());
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`App running in ${port}`);
+});
