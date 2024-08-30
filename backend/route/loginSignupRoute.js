cca0d4359c7020df6e77eebd137a7bcfabafdf28
Sahitya hp <neupanesahitya1@gmail.com>
2024-08-30 00:47:39 -0400

    send the login data to backend

--- backend/route/loginSignupRoute.js
+++ backend/route/loginSignupRoute.js
@@ -3,5 +3,8 @@ const signupController = require("../controllers/signupcontroller");
 const router = express.Router();
 
 router.post("/signup", signupController);
+router.post("/login", (req, res) => {
+  console.log(req.body);
+});
 
 module.exports = router;
--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -3,24 +3,48 @@ import React, { useState } from "react";
 import LoginHeading from "../components/LoginHeading";
 import LoginInput from "../components/LoginInput";
 import LoginButton from "../components/LoginButton";
+import Button from "../components/Button";
+import { backendUrl } from "../utils/constant";
 
 const Page = () => {
   const handleInput = (e) => {
-    setSignupData({ ...signupData, [`${e.target.name}`]: e.target.value });
+    setLoginData({ ...loginData, [`${e.target.name}`]: e.target.value });
   };
-  const [signupData, setSignupData] = useState({
+  const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
-  const handleLogin = () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
     console.log("login is handled");
+    if (loginData.email === "" || loginData.password === "") {
+      console.log("Fill Out the form again");
+      alert("Form is Empty");
+      return null;
+    }
+    try {
+      const response = await fetch(`${backendUrl}login`, {
+        method: "POST",
+        body: JSON.stringify(loginData),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div
       className="flex   
- items-center justify-center h-screen "
+ items-center justify-center h-screen w-full relative"
     >
       {" "}
+      <div className="absolute top-5 left-5">
+        <Button text="Sign Up" link={"/signup"} shadowColor={"pink"} />
+      </div>{" "}
        
       <div className="bg-white p-8 rounded-lg shadow-2xl w-[25rem] border-2 border-solid border-black">
         <LoginHeading text="Login" />
@@ -29,15 +53,15 @@ const Page = () => {
             label={"Email:"}
             handleInput={handleInput}
             type="email"
-            value={signupData.email}
+            value={loginData.email}
           />
           <LoginInput
             label="Password:"
             handleInput={handleInput}
             type={"password"}
-            value={signupData.password}
+            value={loginData.password}
           />
-          <LoginButton text="sign up" handleButton={handleLogin} />
+          <LoginButton text="Login" handleButton={handleLogin} />
         </form>
       </div>
     </div>
